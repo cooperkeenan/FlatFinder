ea604ef3a01ac387884c9bd99da5aba7bab7ba5b
cooperlkeenan <cooperlkeenan@gmail.com>
2024-12-02 15:56:31 +0000

    search page filters all working

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from models import db
 from models import Property
 import json
-from property_filters import filter_properties, clean_price
+from property_filters import filter_price, clean_price, filter_bedrooms, filter_location
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flatfinder.db'
@@ -15,35 +15,45 @@ db.init_app(app)
 def home():
     return render_template('index.html')  
 
-# For You page 
+# For You  
 @app.route('/for-you')
 def for_you():
     return render_template('for_you.html')  
 
-
+# Search Filter Page
 @app.route('/search')
 def search():
     return render_template('search.html')
 
 
+# Search Results 
 @app.route('/search/results')
 def search_results():
+    # Get filter parameters 
     min_price = request.args.get('min_price')
     max_price = request.args.get('max_price')
+    bedrooms = request.args.get('bedrooms')  
+    location = request.args.get('location')  
 
+    # remove symobols 
     min_price = clean_price(min_price) if min_price else None
     max_price = clean_price(max_price) if max_price else None
 
     properties = Property.query.all()
-    filtered_properties = filter_properties(properties, min_price, max_price)
-    return render_template('search_results.html', properties=filtered_properties)
+
+    # Apply filters
+    properties = filter_price(properties, min_price, max_price)
+    properties = filter_bedrooms(properties, bedrooms)
+    properties = filter_location(properties, location)
+
+    return render_template('search_results.html', properties=properties)
+
 
 
 
 @app.route('/viewings')
 def viewings():
     return render_template('viewing_manager.html')
-
 
 
 @app.route('/property/<property_id>')
